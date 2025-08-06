1123c1b80bed1a3bb709c48bfe3b31ed83af0148
gon-fel <agoncalves.felipe.pessoal@gmail.com>
2025-08-06 09:45:05 -0300

    Adicionar exercícios iniciais sobre input de dados

---
+++ exercícios_até_input/exe1.py
@@ -0,0 +1,13 @@
+nome = 'Augusto'
+sobrenome = 'Gonçalves'
+idade = 32
+ano_nascimento = 1992
+maior_idade = idade >= 18
+altura = 1.72
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de Nascimento:", ano_nascimento)
+print("É maior de Idade?", maior_idade)
+print("Sua altura é:", altura, "metros")
---
+++ exercícios_até_input/exe2.py
@@ -0,0 +1,6 @@
+nome = 'Augusto'
+altura = 1.72
+peso = 90
+imc = peso / (altura * altura)
+
+print(nome, 'tem de altura:',altura, 'pesa:',peso, 'e seu imc é de:',imc )
